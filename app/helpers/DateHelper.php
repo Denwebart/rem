67ce5a39d43e35fb1ba09eebb3d7816ee66daa0d
Denwebart <denwebart@yandex.ua>
2015-02-21 17:39:40 +0200

    DateHelper::getRelativeTime ("назад"). Удаление анимации уведомлений в админке.

--- app/helpers/DateHelper.php
+++ app/helpers/DateHelper.php
@@ -58,29 +58,46 @@ class DateHelper
 	public static function getRelativeTime($date)
 	{
 		$delta = (time() - strtotime($date));
+		$endText = ' назад';
 
 		if ($delta < 0) {
 			return '0 сек.';
 		}
 		if ($delta < 60) {
-			$result = round($delta, 0) . ' сек.';
+			$result = round($delta, 0) . ' сек.' . $endText;
 		} elseif ($delta < 120) {
-			$result = '1 мин.';
+			$result = '1 мин.' . $endText;
 		} elseif ($delta < (45 * 60)) {
-			$result = round(($delta / 60), 0) . ' мин.';
+			$result = round(($delta / 60), 0) . ' мин.' . $endText;
 		} elseif ($delta < (2 * 60 * 60)) {
-			$result = '1 час';
-		} elseif ($delta < (5 * 60 * 60)) {
-			$result = round(($delta / 3600), 0) . ' часа';
+			$result = '1 ч.' . $endText;
 		} elseif ($delta < (24 * 60 * 60)) {
-			$result = round(($delta / 3600), 0) . ' часов';
+			$result = round(($delta / 3600), 0) . ' ч.' . $endText;
 		} elseif ($delta < (48 * 60 * 60)) {
-			$result = '1 день';
-		} elseif ($delta < (24 * 5 * 60 * 60)) {
-			$result = round(($delta / 86400), 0) . ' дня';
+			$result = '1 д.' . $endText;
 		} else {
-			$result = round(($delta / 86400), 0) . ' дней';
+			$result = round(($delta / 86400), 0) . ' д.' . $endText;
 		}
+
+//		if ($delta < 60) {
+//			$result = round($delta, 0) . ' сек.' . $endText;
+//		} elseif ($delta < 120) {
+//			$result = '1 мин.' . $endText;
+//		} elseif ($delta < (45 * 60)) {
+//			$result = round(($delta / 60), 0) . ' мин.' . $endText;
+//		} elseif ($delta < (2 * 60 * 60)) {
+//			$result = '1 час' . $endText;
+//		} elseif ($delta < (5 * 60 * 60)) {
+//			$result = round(($delta / 3600), 0) . ' часа' . $endText;
+//		} elseif ($delta < (24 * 60 * 60)) {
+//			$result = round(($delta / 3600), 0) . ' часов' . $endText;
+//		} elseif ($delta < (48 * 60 * 60)) {
+//			$result = '1 день' . $endText;
+//		} elseif ($delta < (24 * 5 * 60 * 60)) {
+//			$result = round(($delta / 86400), 0) . ' дня' . $endText;
+//		} else {
+//			$result = round(($delta / 86400), 0) . ' дней' . $endText;
+//		}
 		return $result;
 	}
 
