476b5d99158bcc51b453dbfd639de890a62e4089
Annywebart <annywebart@yandex.ua>
2015-03-31 23:15:37 +0300

    Отображение рейтинга (звезд) в виджете в левой колонке.

--- app/views/widgets/sidebar/best.blade.php
+++ app/views/widgets/sidebar/best.blade.php
@@ -6,8 +6,30 @@
             <a href="{{ URL::to($page->getUrl()) }}">
                 {{ $page->getTitle() }}
             </a>
-            <div class="views">Оценка: {{ $page->getRating() }}</div>
+            <div class="rate clearfix">
+                <div class="rate-votes pull-left">{{ $page->getRating() }}</div>
+                <div class="rate-stars pull-left">
+                    <div id="jRate-{{ $page->id }}"></div>
+                </div>
+            </div>
+            @section('script')
+                @parent
+                <script type="text/javascript">
+                    $("#jRate-<?php echo $page->id; ?>").jRate({
+                        rating: '<?php echo $page->getRating(); ?>',
+                        readOnly: true,
+                        startColor: '#84BCE6',
+                        endColor: '#2D4C7F'
+                    });
+                </script>
+            @endsection
         </div>
     @endforeach
 
 </div>
+@section('script')
+    @parent
+
+    {{ HTML::script('js/jRate.js') }}
+
+@endsection
