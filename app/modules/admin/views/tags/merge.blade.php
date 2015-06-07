5ec55cb75f9979603ba857dc36675e3bd6733ea5
Annywebart <annywebart@yandex.ua>
2015-06-07 19:19:07 +0300

    Слияние тегов: удаление выбранных.

--- app/modules/admin/views/tags/merge.blade.php
+++ app/modules/admin/views/tags/merge.blade.php
@@ -25,10 +25,11 @@
                         {{ Form::open(['method' => 'POST', 'route' => ['admin.tags.postMerge'], 'id' => 'merge-tags-form']) }}
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <div class="original-tags row">
-                                        {{ Form::label('tags[1]', 'Тег', ['class' => 'col-sm-2 control-label']) }}
-                                        {{ Form::text('tags[1]', null, ['class' => 'form-control autocomplete', 'placeholder' => '']) }}
-                                        {{ $errors->first('tags[1]') }}
+                                    <div class="original-tags">
+                                        {{--{{ Form::label('tags[1]', 'Тег', ['class' => 'col-sm-2 control-label']) }}--}}
+                                        <div class="input first">
+                                            {{ Form::text('tags[1]', null, ['class' => 'form-control', 'placeholder' => '', 'disabled' => 'disabled', 'id' => 'tags[1]']) }}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -91,7 +92,7 @@
 
     <script type="text/javascript">
 
-        $('#merge-tags-form').trigger('reset');
+        $('#merge-tags-form, #search-tags-form').trigger('reset');
 
         $(".autocomplete").autocomplete({
             source: "<?php echo URL::route('admin.tags.autocomplete') ?>",
@@ -136,17 +137,38 @@
         var inputNumber = 1;
         $("#search-result").on('click', '.add-to-input', function() {
             var $link = $(this);
-            var linkText = $link.find('.text').text();
+            var linkText = $link.find('.text').text().trim();
+            var deleteInputHtml = '<a type="button" class="btn btn-danger btn-circle delete-input"><i class="glyphicon glyphicon-remove"></i></a>';
             if(inputNumber == 1) {
                 $("[id^='tags']").val(linkText);
+                $("[id^='tags']").parent().append(deleteInputHtml);
             } else {
-                var plusHtml = '<button type="button" class="btn btn-default btn-circle btn-outline"><i class="glyphicon glyphicon-plus"></i></button>';
-                var inputHtml = '<input value="'+ linkText +'" class="form-control autocomplete ui-autocomplete-input" placeholder="" name="tags['+inputNumber+']" id="tags['+inputNumber+']" type="text">';
-                var deleteInputHtml = '<a type="button" class="btn btn-danger btn-circle"><i class="glyphicon glyphicon-remove"></i></a>';
-                $('.original-tags').append(plusHtml + inputHtml + deleteInputHtml);
+                var plusHtml = '<button type="button" class="btn btn-default btn-circle btn-outline plus"><i class="glyphicon glyphicon-plus"></i></button>';
+                var inputHtml = '<input value="'+ linkText +'" class="form-control autocomplete ui-autocomplete-input" placeholder="" name="tags['+inputNumber+']" id="tags['+inputNumber+']" type="text" disabled="disabled">';
+                $('.original-tags').append('<div class="input">' + plusHtml + inputHtml + deleteInputHtml + '</div>');
             }
             inputNumber++;
             $link.remove();
+        });
+
+        $(".original-tags").on('click', '.delete-input', function() {
+            var $deleteLink = $(this);
+            if(!$deleteLink.parent().hasClass('first')) {
+                $deleteLink.parent().remove();
+                inputNumber--;
+            } else {
+                if(inputNumber <= 2) {
+                    $deleteLink.parent().find('input').val('');
+                    $deleteLink.remove();
+                    inputNumber = 1;
+                } else {
+                    $deleteLink.parent().remove();
+                    inputNumber--;
+                    $(".original-tags").find('.input').first().addClass('first');
+                    $(".original-tags").find('.input').first().find('.plus').remove();
+                    $(".original-tags").find('.input').first().find('input').attr('id', 'tags[1]').attr('name', 'tags[1]');
+                }
+            }
         });
 
     </script>
