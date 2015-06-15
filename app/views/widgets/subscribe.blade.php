d1b27fd3c4679e646e38d3762de47e344d297d11
Denwebart <denwebart@yandex.ua>
2015-06-15 13:47:45 +0300

    Исправление ошибки с подписками для неавторизованных пользователей (на странице с вопросом была 404).

--- app/views/widgets/subscribe.blade.php
+++ app/views/widgets/subscribe.blade.php
@@ -1,74 +1,78 @@
 <div class="row">
     <div class="col-md-12">
         <div id="subscription">
-            @if(!Auth::user()->subscribed($page->id))
-                <a href="javascript:void(0)" id="subscribe" data-page-id="{{ $page->id }}" class="btn btn-success pull-right">
+            @if(Auth::check())
+                @if(!Auth::user()->subscribed($page->id))
+                    <a href="javascript:void(0)" id="subscribe" data-page-id="{{ $page->id }}" class="btn btn-success pull-right">
                     <span class="text-link">
                         Подписаться
                     </span>
                     <span class="subscribers">
                         {{ count($page->subscribers) }}
                     </span>
-                </a>
-            @else
-                <a href="javascript:void(0)" id="unsubscribe" data-page-id="{{ $page->id }}" class="btn btn-success pull-right">
+                    </a>
+                @else
+                    <a href="javascript:void(0)" id="unsubscribe" data-page-id="{{ $page->id }}" class="btn btn-success pull-right">
                     <span class="text-link">
                         Вы подписаны
                     </span>
                     <span class="subscribers">
                         {{ count($page->subscribers) }}
                     </span>
-                </a>
+                    </a>
+                @endif
             @endif
         </div>
         <div id="subscribe-message"></div>
     </div>
 </div>
 
-@section('script')
-    @parent
+@if(Auth::check())
+    @section('script')
+        @parent
 
-    <script type="text/javascript">
-        $("#subscription").on('click', '#subscribe', function() {
-            var $link = $(this);
-            var pageId = $link.data('pageId');
-            $.ajax({
-                url: "{{ URL::route('user.subscribe', ['login' => Auth::user()->getLoginForUrl()]) }}",
-                dataType: "text json",
-                type: "POST",
-                data: {pageId: pageId},
-                success: function(response) {
-                    if(response.success){
-                        $("#subscribe-message").text(response.message);
-                        $link.find('.text-link').text('Вы подписаны');
-                        $link.find('.subscribers').text(response.subscribers);
-                        $link.attr('id', 'unsubscribe');
-                    } else {
-                        $("#subscribe-message").text(response.message);
+        <script type="text/javascript">
+            $("#subscription").on('click', '#subscribe', function() {
+                var $link = $(this);
+                var pageId = $link.data('pageId');
+                $.ajax({
+                    url: "{{ URL::route('user.subscribe', ['login' => Auth::user()->getLoginForUrl()]) }}",
+                    dataType: "text json",
+                    type: "POST",
+                    data: {pageId: pageId},
+                    success: function(response) {
+                        if(response.success){
+                            $("#subscribe-message").text(response.message);
+                            $link.find('.text-link').text('Вы подписаны');
+                            $link.find('.subscribers').text(response.subscribers);
+                            $link.attr('id', 'unsubscribe');
+                        } else {
+                            $("#subscribe-message").text(response.message);
+                        }
                     }
-                }
+                });
             });
-        });
 
-        $("#subscription").on('click', '#unsubscribe', function() {
-            var $link = $(this);
-            var pageId = $link.data('pageId');
-            $.ajax({
-                url: "{{ URL::route('user.unsubscribe', ['login' => Auth::user()->getLoginForUrl()]) }}",
-                dataType: "text json",
-                type: "POST",
-                data: {pageId: pageId},
-                success: function(response) {
-                    if(response.success){
-                        $("#subscribe-message").text(response.message);
-                        $link.find('.text-link').text('Подписаться');
-                        $link.find('.subscribers').text(response.subscribers);
-                        $link.attr('id', 'subscribe');
-                    } else {
-                        $("#subscribe-message").text(response.message);
+            $("#subscription").on('click', '#unsubscribe', function() {
+                var $link = $(this);
+                var pageId = $link.data('pageId');
+                $.ajax({
+                    url: "{{ URL::route('user.unsubscribe', ['login' => Auth::user()->getLoginForUrl()]) }}",
+                    dataType: "text json",
+                    type: "POST",
+                    data: {pageId: pageId},
+                    success: function(response) {
+                        if(response.success){
+                            $("#subscribe-message").text(response.message);
+                            $link.find('.text-link').text('Подписаться');
+                            $link.find('.subscribers').text(response.subscribers);
+                            $link.attr('id', 'subscribe');
+                        } else {
+                            $("#subscribe-message").text(response.message);
+                        }
                     }
-                }
+                });
             });
-        });
-    </script>
-@endsection
+        </script>
+    @stop
+@endif
