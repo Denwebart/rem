8e22462c4b8b76922fce058a2057cf1e0e52be5d
Denwebart <denwebart@yandex.ua>
2015-07-02 17:59:06 +0300

    Запрет регистрации с забаненного ip-адреса.

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -13,35 +13,33 @@ View::share('title', $title);
 
                 <h2>Регистрация</h2>
 
-                {{ Form::open(['url' => 'register_request', 'role' => 'form', 'class' => '']) }}
-
-                    <div class="form-group">
-                        {{ Form::text('login', null, array('class' => 'form-control floating-label', 'placeholder' => 'Логин*', 'autofocus'=>'autofocus')) }}
-                        @if ($errors->has('login')) <p class="text-danger">{{ $errors->first('login') }}</p> @endif
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::text('email', null, array('class' => 'form-control floating-label', 'placeholder' => 'E-Mail*')) }}
-                        @if ($errors->has('email')) <p class="text-danger">{{ $errors->first('email') }}</p> @endif
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::password('password', array('class' => 'form-control floating-label', 'placeholder' => 'Пароль*')) }}
-                        @if ($errors->has('password')) <p class="text-danger">{{ $errors->first('password') }}</p> @endif
-                    </div>
-
-                    <div class="form-group">
-                        {{ Form::password('password_confirmation', array('class' => 'form-control floating-label', 'placeholder' => 'Повтор пароля*')) }}
-                        @if ($errors->has('password_confirmation')) <p class="text-danger">{{ $errors->first('password_confirmation') }}</p> @endif
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-sm-5 col-sm-offset-2">
-                            {{ Form::submit('Зарегистрироваться', ['id'=> 'submit', 'class' => 'btn btn-primary']) }}
+                @if(!Ip::isBanned())
+                    {{ Form::open(['url' => 'register_request', 'role' => 'form', 'class' => '']) }}
+                        <div class="form-group">
+                            {{ Form::text('login', null, array('class' => 'form-control floating-label', 'placeholder' => 'Логин*', 'autofocus'=>'autofocus')) }}
+                            @if ($errors->has('login')) <p class="text-danger">{{ $errors->first('login') }}</p> @endif
                         </div>
-                    </div>
-
-                {{ Form::close() }}
+                        <div class="form-group">
+                            {{ Form::text('email', null, array('class' => 'form-control floating-label', 'placeholder' => 'E-Mail*')) }}
+                            @if ($errors->has('email')) <p class="text-danger">{{ $errors->first('email') }}</p> @endif
+                        </div>
+                        <div class="form-group">
+                            {{ Form::password('password', array('class' => 'form-control floating-label', 'placeholder' => 'Пароль*')) }}
+                            @if ($errors->has('password')) <p class="text-danger">{{ $errors->first('password') }}</p> @endif
+                        </div>
+                        <div class="form-group">
+                            {{ Form::password('password_confirmation', array('class' => 'form-control floating-label', 'placeholder' => 'Повтор пароля*')) }}
+                            @if ($errors->has('password_confirmation')) <p class="text-danger">{{ $errors->first('password_confirmation') }}</p> @endif
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-5 col-sm-offset-2">
+                                {{ Form::submit('Зарегистрироваться', ['id'=> 'submit', 'class' => 'btn btn-primary']) }}
+                            </div>
+                        </div>
+                    {{ Form::close() }}
+                @else
+                    @include('messages.bannedIp')
+                @endif
             </div>
         </div>
     </div>
