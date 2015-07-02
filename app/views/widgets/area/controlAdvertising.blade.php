a80668f2b31de94940ea01487c38b9b19b229a22
Denwebart <denwebart@yandex.ua>
2015-07-02 20:42:38 +0300

    Стили кнопок при редактировании рекламы.

--- app/views/widgets/area/controlAdvertising.blade.php
+++ app/views/widgets/area/controlAdvertising.blade.php
@@ -2,11 +2,11 @@
     <div class="access">
         {{ Advertising::$access[$item->access] }}
     </div>
-    <a href="{{ URL::route('admin.advertising.edit', ['id' => $item->id, 'backUrl' => urlencode(Request::url())]) }}" class="btn btn-info btn-sm">
+    <a href="{{ URL::route('admin.advertising.edit', ['id' => $item->id, 'backUrl' => urlencode(Request::url())]) }}" class="btn btn-info btn-sm" title="Редактировать этот рекламный блок/виджет">
         <span class="mdi-editor-mode-edit"></span>
     </a>
     <!-- Отключить/выключить рекламный блок -->
-    <a href="javascript:void(0)" class="btn btn-warning btn-sm change-active-status" data-id="{{ $item->id }}" data-is-active="{{ $item->is_active }}" title="{{ $item->is_active ? 'Выключить этот рекламный блок на этой старинце.' : 'Включить этот рекламный блок на этой старинце.' }}">
+    <a href="javascript:void(0)" class="btn btn-warning btn-sm change-active-status" data-id="{{ $item->id }}" data-is-active="{{ $item->is_active }}" title="{{ $item->is_active ? 'Выключить этот рекламный блок/виджет' : 'Включить этот рекламный блок/виджет' }}">
         @if($item->is_active)
             <span class="mdi-action-visibility-off"></span>
         @else
--- app/views/widgets/area/create.blade.php
+++ app/views/widgets/area/create.blade.php
@@ -2,7 +2,7 @@
     {{ Advertising::$areas[$area] }}
 </div>
 <div class="buttons" style="display: none">
-    <a href="{{ URL::route('admin.advertising.create', ['backUrl' => urlencode(Request::url()), 'area' => $area]) }}" class="btn btn-success btn-sm create">
+    <a href="{{ URL::route('admin.advertising.create', ['backUrl' => urlencode(Request::url()), 'area' => $area]) }}" class="btn btn-success btn-sm create" title="Создать рекламный блок/виджет в этой области">
         <span class="mdi-content-add"></span>
     </a>
 </div>
