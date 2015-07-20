16049df28df2b190acb6fc33779548448dc10ee8
Denwebart <denwebart@yandex.ua>
2015-07-20 17:51:39 +0300

    Вывод баллов пользователя в профиле.

--- app/modules/cabinet/views/user/index.blade.php
+++ app/modules/cabinet/views/user/index.blade.php
@@ -20,126 +20,147 @@ View::share('title', $title);
 
         <div class="row">
             <div class="col-md-12" id="content">
-                @if(Session::has('rulesSuccessMessage'))
-                    <div class="alert alert-dismissable alert-success">
-                        <button type="button" class="close" data-dismiss="alert">×</button>
-                        {{ Session::get('rulesSuccessMessage') }}
-                    </div>
-                @endif
-
-                @if(Auth::check())
-                    @if(Auth::user()->is($user) && !Auth::user()->is_agree)
-                        @include('messages.rulesAgree')
-                    @endif
-
-                    @if(Auth::user()->is($user) && Auth::user()->is_banned)
-                        @include('cabinet::user.banMessage')
-                    @endif
-
-                    @if(Auth::user()->is($user) && $headerWidget->isBannedIp)
-                        @include('messages.bannedIp')
-                    @endif
-                @endif
-
-                @if(Auth::check())
-                    @if((Auth::user()->is($user) && !$headerWidget->isBannedIp && !$user->is_banned) || Auth::user()->isAdmin())
-                        <a href="{{{ URL::route('user.edit', ['login' => $user->getLoginForUrl()]) }}}" class="pull-right">
-                            Редактировать
-                            <span class="mdi-editor-border-color"></span>
-                        </a>
-                    @endif
-                @endif
-
-                @if(Auth::check())
-                    @if(Auth::user()->is($user))
-                        <div class="clearfix"></div>
-                        <a href="{{{ URL::route('user.changePassword', ['login' => $user->getLoginForUrl()]) }}}" class="pull-right">
-                            Изменить пароль
-                            <!-- mdi-communication-vpn-key -->
-                            <!-- mdi-action-verified-user -->
-                            <span class="mdi-hardware-security"></span>
-                        </a>
-                    @endif
-                @endif
-
-                @if(Session::has('successMessage'))
-                    <div class="alert alert-dismissable alert-info">
-                        <button type="button" class="close" data-dismiss="alert">×</button>
-                        {{ Session::get('successMessage') }}
-                    </div>
-                @endif
-
-                <h2>{{{ $user->login }}}</h2>
-
-                <p class="date date-register">
-                    <span>Дата регистрации: </span>
-                    {{{ DateHelper::dateFormat($user->created_at) }}}
-                </p>
-
-                @if(Auth::check())
-                    @if(Auth::user()->is($user) || Auth::user()->isAdmin() || Auth::user()->isModerator())
-                        <p class="email">{{{ $user->email }}}</p>
-                    @endif
-                @endif
-
-                @if($user->isAdmin() || $user->isModerator())
-                    <p>{{ User::$roles[$user->role] }}</p>
-                @endif
-
-                @if($user->getFullName())
-                    <h3>{{{ $user->getFullName() }}}</h3>
-                @endif
-
-                @if($user->country)
-                    <p>{{{ $user->country }}}</p>
-                @endif
-
-                @if($user->city)
-                    <p>{{{ $user->city }}}</p>
-                @endif
-
-                @if($user->car_brand)
-                    <p>{{{ $user->car_brand }}}</p>
-                @endif
-
-                @if($user->profession)
-                    <p>{{{ $user->profession }}}</p>
-                @endif
-
-                @if($user->profession)
-                    <p>{{ $user->description }}</p>
-                @endif
-
-                <h2 id="honors">Награды</h2>
-
-                @if(count($user->honors))
-                    <div id="message"></div>
-                    @foreach($user->honors as $honor)
-                        <div class="honor">
-                            <a href="{{ URL::route('honor.info', ['alias' => $honor->alias]) }}">
-                                {{ $honor->getImage(null, ['width' => '75px']) }}
-                            </a>
-                            @if(Auth::check())
-                                @if(Auth::user()->isAdmin())
-                                    <a href="javascript:void(0)" class="btn btn-danger btn-sm remove-reward" data-honor-id="{{ $honor->id }}">
-                                        <span class="mdi-content-clear"></span>
-                                    </a>
-                                @endif
-                            @endif
-                        </div>
-                    @endforeach
-                @else
-                    @if(Auth::check())
-                        @if(!Auth::user()->is($user))
-                            Нет наград.
-                        @else
-                            У Вас нет наград. Узнать о том, как можно получить награду, можно
-                            <a href="">здесь</a>.
+                <div class="row">
+                    <div class="col-md-8">
+                        @if(Session::has('rulesSuccessMessage'))
+                            <div class="alert alert-dismissable alert-success">
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+                                {{ Session::get('rulesSuccessMessage') }}
+                            </div>
                         @endif
-                    @else
-                        Нет наград.
-                    @endif
-                @endif
+
+                        @if(Auth::check())
+                            @if(Auth::user()->is($user) && !Auth::user()->is_agree)
+                                @include('messages.rulesAgree')
+                            @endif
+
+                            @if(Auth::user()->is($user) && Auth::user()->is_banned)
+                                @include('cabinet::user.banMessage')
+                            @endif
+
+                            @if(Auth::user()->is($user) && $headerWidget->isBannedIp)
+                                @include('messages.bannedIp')
+                            @endif
+                        @endif
+
+                        @if(Session::has('successMessage'))
+                            <div class="alert alert-dismissable alert-info">
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+                                {{ Session::get('successMessage') }}
+                            </div>
+                        @endif
+
+                        <h2>{{{ $user->login }}}</h2>
+
+                        <p class="date date-register">
+                            <span>Дата регистрации: </span>
+                            {{{ DateHelper::dateFormat($user->created_at) }}}
+                        </p>
+
+                        @if(Auth::check())
+                            @if(Auth::user()->is($user) || Auth::user()->isAdmin() || Auth::user()->isModerator())
+                                <p class="email">{{{ $user->email }}}</p>
+                            @endif
+                        @endif
+
+                        @if($user->isAdmin() || $user->isModerator())
+                            <p>{{ User::$roles[$user->role] }}</p>
+                        @endif
+
+                        @if($user->getFullName())
+                            <h3>{{{ $user->getFullName() }}}</h3>
+                        @endif
+
+                        @if($user->country)
+                            <p>{{{ $user->country }}}</p>
+                        @endif
+
+                        @if($user->city)
+                            <p>{{{ $user->city }}}</p>
+                        @endif
+
+                        @if($user->car_brand)
+                            <p>{{{ $user->car_brand }}}</p>
+                        @endif
+
+                        @if($user->profession)
+                            <p>{{{ $user->profession }}}</p>
+                        @endif
+                    </div>
+                    <div class="col-md-4">
+                        <div class="row">
+                            <div class="col-md-12">
+                                @if(Auth::check())
+                                    @if((Auth::user()->is($user) && !$headerWidget->isBannedIp && !$user->is_banned) || Auth::user()->isAdmin())
+                                        <a href="{{{ URL::route('user.edit', ['login' => $user->getLoginForUrl()]) }}}" class="pull-right">
+                                            Редактировать
+                                            <span class="mdi-editor-border-color"></span>
+                                        </a>
+                                    @endif
+                                @endif
+                            </div>
+                            <div class="col-md-12">
+                                @if(Auth::check())
+                                    @if(Auth::user()->is($user))
+                                        <a href="{{{ URL::route('user.changePassword', ['login' => $user->getLoginForUrl()]) }}}" class="pull-right">
+                                            Изменить пароль
+                                            <!-- mdi-communication-vpn-key -->
+                                            <!-- mdi-action-verified-user -->
+                                            <span class="mdi-hardware-security"></span>
+                                        </a>
+                                    @endif
+                                @endif
+                            </div>
+                            <div class="col-md-12">
+                                <div class="well margin-top-20">
+                                    <h4>
+                                        Баллов:
+                                        <span class="count">
+                                            {{ $user->points }}
+                                        </span>
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        @if($user->description)
+                            <p>{{ $user->description }}</p>
+                        @endif
+                    </div>
+                    <div class="col-md-12">
+                        <h2 id="honors">Награды</h2>
+
+                        @if(count($user->honors))
+                            <div id="message"></div>
+                            @foreach($user->honors as $honor)
+                                <div class="honor">
+                                    <a href="{{ URL::route('honor.info', ['alias' => $honor->alias]) }}">
+                                        {{ $honor->getImage(null, ['width' => '75px']) }}
+                                    </a>
+                                    @if(Auth::check())
+                                        @if(Auth::user()->isAdmin())
+                                            <a href="javascript:void(0)" class="btn btn-danger btn-sm remove-reward" data-honor-id="{{ $honor->id }}">
+                                                <span class="mdi-content-clear"></span>
+                                            </a>
+                                        @endif
+                                    @endif
+                                </div>
+                            @endforeach
+                        @else
+                            @if(Auth::check())
+                                @if(!Auth::user()->is($user))
+                                    Нет наград.
+                                @else
+                                    У Вас нет наград. Узнать о том, как можно получить награду, можно
+                                    <a href="">здесь</a>.
+                                @endif
+                            @else
+                                Нет наград.
+                            @endif
+                        @endif
+                    </div>
+                </div>
             </div>
             <div class="col-md-12">
                 {{ $areaWidget->contentBottom() }}
