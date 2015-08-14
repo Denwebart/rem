2218cee6bd17c1d31ceb5186254af60937d9870e
Denwebart <denwebart@yandex.ua>
2015-08-14 17:23:08 +0300

    Кнопка "Наверх". Стили футера и нижнего меню.

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -1,14 +1,64 @@
-<footer class="container">
-	<div class="row">
-		<div class="col-xs-12">
-			{{ $menuWidget->bottomMenu() }}
-		</div>
-		<div class="col-md-12">
-            @if(isset($settings))
-                {{ $settings['copyright']['value'] }}
-            @endif
-		</div>
-	</div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                {{ $menuWidget->bottomMenu() }}
+            </div>
+
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+
+            <div class="col-md-12">
+                <!-- Копирайт -->
+                @if(isset($settings))
+                    <div class="copyright">
+                        {{ $settings['copyright']['value'] }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
 </footer>
 
 @include('widgets.siteMessages.siteMessages')
+
+<!-- Back To Top -->
+<a href="javascript:void(0)" class="back-to-top">
+    <i class="material-icons">keyboard_arrow_up</i>
+</a>
+
+@section('script')
+    @parent
+
+    <script type="text/javascript">
+        $(document).ready(function($){
+            // browser window scroll (in pixels) after which the "back to top" link is shown
+            var offset = 300,
+            //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+                    offset_opacity = 1200,
+            //duration of the top scrolling animation (in ms)
+                    scroll_top_duration = 500,
+            //grab the "back to top" link
+                    $back_to_top = $('.back-to-top');
+
+            //hide or show the "back to top" link
+            $(window).scroll(function(){
+                ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible fade-out');
+                if( $(this).scrollTop() > offset_opacity ) {
+                    $back_to_top.addClass('fade-out');
+                }
+            });
+
+            //smooth scroll to top
+            $back_to_top.on('click', function(event){
+                event.preventDefault();
+                $('body,html').animate({
+                            scrollTop: 0
+                        }, scroll_top_duration
+                );
+            });
+
+        });
+    </script>
+@endsection
--- app/views/widgets/menu/bottom.blade.php
+++ app/views/widgets/menu/bottom.blade.php
@@ -1,13 +1,11 @@
-<nav class="navbar navbar-custom">
-    <div id="navbar-bottom" class="navbar-collapse collapse navbar-responsive-collapse">
-        <ul class="nav navbar-nav">
-            @foreach($pages as $page)
-                <li class="{{ (Request::is($page->alias . '/*') || Request::is($page->getUrl())) ? 'active' : '' }}">
-                    <a href="{{ URL::to($page->getUrl()) }}">
-                        {{ $page->getTitle() }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-    </div><!--/.nav-collapse -->
+<nav>
+    <ul id="navbar-bottom">
+        @foreach($pages as $page)
+            <li class="{{ (Request::is($page->alias . '/*') || Request::is($page->getUrl())) ? 'active' : '' }}">
+                <a href="{{ URL::to($page->getUrl()) }}">
+                    {{ $page->getTitle() }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
 </nav>
