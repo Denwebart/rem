6d7920b1eb4358c5a523c6fa9e96c229b4d51dec
Annywebart <annywebart@yandex.ua>
2015-08-24 01:50:04 +0300

    Исправление стилей кнопок rss, поиска, добавления в закладки.

--- app/views/widgets/sidebar/rss.blade.php
+++ app/views/widgets/sidebar/rss.blade.php
@@ -1,5 +1,6 @@
 <div id="rss-widget" class="sidebar-widget">
     <a href="{{ URL::to('rss') }}" class="btn btn-default btn-raised">
+        {{ HTML::image('images/rss.png') }}
         <span>RSS</span>
     </a>
 </div>
