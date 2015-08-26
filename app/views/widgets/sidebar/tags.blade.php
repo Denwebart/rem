02a1f6fdf8b4c9f9cf33ebbb7aff697f43709f0f
Denwebart <denwebart@yandex.ua>
2015-08-26 20:52:10 +0300

    Стили виджета с тегами.

--- app/views/widgets/sidebar/tags.blade.php
+++ app/views/widgets/sidebar/tags.blade.php
@@ -1,10 +1,10 @@
 <div id="tags-sidebar-widget" class="sidebar-widget">
     @foreach($tags as $tag)
-        <a href="{{ URL::route('journal.tag', ['journalAlias' => Config::get('settings.journalAlias'), 'tag' => $tag->title]) }}" title="{{ $tag->title }}" class="tag btn btn-sm btn-info">
+        <a href="{{ URL::route('journal.tag', ['journalAlias' => Config::get('settings.journalAlias'), 'tag' => $tag->title]) }}" title="{{ $tag->title }}" class="tag btn btn-sm btn-default">
             {{ $tag->title }} ({{ count($tag->pages) }})
         </a>
     @endforeach
-    <div>
+    <div class="all-tags">
         <a href="{{ URL::route('journal.tags', ['journalAlias' => Config::get('settings.journalAlias')]) }}">Все теги</a>
     </div>
 </div>
