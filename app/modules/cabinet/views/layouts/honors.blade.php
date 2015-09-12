291cd49632ad5ce4d566375b7dd91d29a091c239
Annywebart <annywebart@yandex.ua>
2015-09-12 04:52:46 +0300

    Адаптивность страниц с наградами, адаптивность TinyMCE.

--- app/modules/cabinet/views/layouts/honors.blade.php
+++ app/modules/cabinet/views/layouts/honors.blade.php
@@ -201,24 +201,6 @@
 <script>
     $(function() {
         $.material.init();
-        $(".shor").noUiSlider({
-            start: 40,
-            connect: "lower",
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
-
-        $(".svert").noUiSlider({
-            orientation: "vertical",
-            start: 40,
-            connect: "lower",
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
     });
 </script>
 
