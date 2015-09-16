65f19e7893502e97adb61c35a492a2d3c0a937be
Annywebart <annywebart@yandex.ua>
2015-09-16 03:18:54 +0300

    Логотип в футере

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -4,11 +4,15 @@
             <div class="col-md-12">
                 {{ $menuWidget->bottomMenu() }}
             </div>
-
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
-
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3">
+                <a href="{{ URL::to('/') }}">
+                    {{ HTML::image('images/logo-footer.png', '', ['class' => 'img-responsive margin-bottom-20']) }}
+                </a>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-md-12">
                 <!-- Копирайт -->
                 @if(isset($settings['copyright']))
