2fca0d655bb166c515ede075fd402d0f2b2d8258
Denwebart <denwebart@yandex.ua>
2015-09-18 21:03:04 +0300

    Исправлена старая миграция.

--- app/database/migrations/2015_08_12_174307_create_table_notifications_messages.php
+++ app/database/migrations/2015_08_12_174307_create_table_notifications_messages.php
@@ -8,7 +8,7 @@ class CreateTableNotificationsMessages extends Migration {
 	public function up()
 	{
 		Schema::create('notifications_messages', function (Blueprint $table) {
-			$table->integer('id');
+			$table->increments('id');
 			$table->string('message', 1000);
 			$table->string('desctiption', 500)->nullable();
 			$table->primary('id');
