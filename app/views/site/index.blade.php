dfc79532fe9cae230c39b6604365218e4e30efa6
Annywebart <annywebart@yandex.ua>
2015-10-10 03:37:12 +0300

    Текст на главной странице убрать при пагинации.

--- app/views/site/index.blade.php
+++ app/views/site/index.blade.php
@@ -2,39 +2,42 @@
 
 @section('content')
 	<section id="content" class="well">
-        <div class="row">
-            <div class="@if($page->showRating()) col-lg-9 col-md-12 col-sm-9 col-xs-12 @else col-lg-12 col-md-12 col-sm-12 col-xs-12 @endif">
-                @if($page->title)
-                    <h2>{{ $page->title }}</h2>
+
+        @if(!Request::has('stranitsa') || Request::get('stranitsa') == 1)
+            <div class="row">
+                <div class="@if($page->showRating()) col-lg-9 col-md-12 col-sm-9 col-xs-12 @else col-lg-12 col-md-12 col-sm-12 col-xs-12 @endif">
+                    @if($page->title)
+                        <h2>{{ $page->title }}</h2>
+                    @endif
+                </div>
+                @if($page->showRating())
+                    <div class="col-lg-3 col-md-12 col-sm-3 col-xs-12">
+                        {{-- Рейтинг --}}
+                        @include('widgets.rating')
+                    </div>
                 @endif
             </div>
-            @if($page->showRating())
-                <div class="col-lg-3 col-md-12 col-sm-3 col-xs-12">
-                    {{-- Рейтинг --}}
-                    @include('widgets.rating')
+
+            <div class="clearfix"></div>
+
+            {{ $areaWidget->contentTop() }}
+
+            @if($page->content)
+                <div class="content">
+                    @if($page->image)
+                        <a class="fancybox" rel="group-content" href="{{ $page->getImageLink('origin') }}">
+                            {{ $page->getImage('origin', ['class' => 'page-image']) }}
+                        </a>
+                    @endif
+                    {{ $page->getContentWithWidget() }}
+
+                    <div class="clearfix"></div>
+                    @include('widgets.sidebar.socialButtons')
                 </div>
             @endif
-        </div>
 
-        <div class="clearfix"></div>
-
-		{{ $areaWidget->contentTop() }}
-
-		@if($page->content)
-			<div class="content">
-                @if($page->image)
-                    <a class="fancybox" rel="group-content" href="{{ $page->getImageLink('origin') }}">
-                        {{ $page->getImage('origin', ['class' => 'page-image']) }}
-                    </a>
-                @endif
-				{{ $page->getContentWithWidget() }}
-
-                <div class="clearfix"></div>
-                @include('widgets.sidebar.socialButtons')
-			</div>
-		@endif
-
-		{{ $areaWidget->contentMiddle() }}
+            {{ $areaWidget->contentMiddle() }}
+        @endif
 
 		<section id="blog-area" class="blog margin-top-10">
             <div class="count">
