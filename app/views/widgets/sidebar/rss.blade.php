ff7067e80eae699e80030cb82103796ddc5f61a3
Annywebart <annywebart@yandex.ua>
2015-11-10 17:06:07 +0200

    Стили кнопок "В закладки" и "Rss".

--- app/views/widgets/sidebar/rss.blade.php
+++ app/views/widgets/sidebar/rss.blade.php
@@ -1,6 +1,8 @@
 <div id="rss-widget" class="sidebar-widget">
     <a href="{{ URL::to('rss') }}" title="Подписка на RSS-ленту" data-toggle="tooltip" class="">
-        {{ HTML::image('images/rss.png') }}
-        <span>RSS</span>
+        <span class="text">
+            {{ HTML::image('images/rss.png') }}
+            <span>RSS</span>
+        </span>
     </a>
 </div>
