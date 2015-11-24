6c94d59115f0c8ece3ed0d54ce5a221df9796c03
Annywebart <annywebart@yandex.ua>
2015-11-24 06:09:02 +0200

    Стили блока "Поделиться" (большие кнопки).

--- app/views/widgets/sidebar/socialButtons.blade.php
+++ app/views/widgets/sidebar/socialButtons.blade.php
@@ -1,4 +1,4 @@
 <div id="social-buttons-widget" class="sidebar-widget">
     <script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script>
-    <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="small" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,gplus" data-yashareTheme="counter"></div>
+    <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,gplus" data-yashareTheme="counter"></div>
 </div>
