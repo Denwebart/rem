5ec4d5a82080fe0ef5726ff6b9df8bedef92d0eb
Annywebart <annywebart@yandex.ua>
2015-11-26 06:08:16 +0200

    Исправление ошибки в рекламных блоках (кнопки редактирования видны в HTML).

--- app/views/widgets/area/controlAdvertising.blade.php
+++ app/views/widgets/area/controlAdvertising.blade.php
@@ -1,17 +1,21 @@
-<div class="buttons pull-right" style="display: none">
-    <div class="access">
-        {{ Advertising::$access[$item->access] }}
-    </div>
-    <a href="{{ URL::route('admin.advertising.edit', ['id' => $item->id, 'backUrl' => urlencode(Request::url())]) }}" class="" title="Редактировать этот рекламный блок/виджет" data-toggle="tooltip">
-        <i class="material-icons">edit</i>
-    </a>
-    <!-- Отключить/выключить рекламный блок -->
-    <a href="javascript:void(0)" class="change-active-status" data-id="{{ $item->id }}" data-is-active="{{ $item->is_active }}" title="{{ $item->is_active ? 'Выключить этот рекламный блок/виджет' : 'Включить этот рекламный блок/виджет' }}" data-toggle="tooltip">
-        @if($item->is_active)
-            <i class="material-icons">visibility_off</i>
-        @else
-            <i class="material-icons">visibility</i>
-        @endif
-    </a>
-</div>
-<div class="clearfix"></div>
+@if(Auth::check())
+    @if(Auth::user()->isAdmin())
+        <div class="buttons pull-right" style="display: none">
+            <div class="access">
+                {{ Advertising::$access[$item->access] }}
+            </div>
+            <a href="{{ URL::route('admin.advertising.edit', ['id' => $item->id, 'backUrl' => urlencode(Request::url())]) }}" class="" title="Редактировать этот рекламный блок/виджет" data-toggle="tooltip">
+                <i class="material-icons">edit</i>
+            </a>
+            <!-- Отключить/выключить рекламный блок -->
+            <a href="javascript:void(0)" class="change-active-status" data-id="{{ $item->id }}" data-is-active="{{ $item->is_active }}" title="{{ $item->is_active ? 'Выключить этот рекламный блок/виджет' : 'Включить этот рекламный блок/виджет' }}" data-toggle="tooltip">
+                @if($item->is_active)
+                    <i class="material-icons">visibility_off</i>
+                @else
+                    <i class="material-icons">visibility</i>
+                @endif
+            </a>
+        </div>
+        <div class="clearfix"></div>
+    @endif
+@endif
