71c322690cde817de636138ec95c8ece1b1f54ca
Annywebart <annywebart@yandex.ua>
2015-11-26 14:03:52 +0200

    Исправление валидности рекламных блоков в контенте (для неавторизованных).

--- app/views/widgets/area/inContent.blade.php
+++ app/views/widgets/area/inContent.blade.php
@@ -1,33 +1,40 @@
 @foreach($advertising as $item)
-    <div class="row">
-        <div class="area area-content" style="margin: 0">
-            @if(Auth::check())
-                @if(Auth::user()->isAdmin())
-                    <div class="widget access-{{ $item->access }}{{ $item->is_active ? '' : ' not-active'}}" {{ $item->is_active ? '' : 'style="display: none"'}} data-widget-id="{{ $item->id }}">
-                    <div class="widget-title" style="display: none">
-                        <a href="{{ URL::route('admin.advertising.index', ['id' => $item->id]) }}" title="Смотреть в админке" data-toggle="tooltip">
-                            {{ $item->title }}
-                        </a>
-                    </div>
+    @if(Auth::check())
+        <div class="row">
+            <div class="area area-content" style="margin: 0">
+                @if(Auth::check())
+                    @if(Auth::user()->isAdmin())
+                        <div class="widget access-{{ $item->access }}{{ $item->is_active ? '' : ' not-active'}}" {{ $item->is_active ? '' : 'style="display: none"'}} data-widget-id="{{ $item->id }}">
+                        <div class="widget-title" style="display: none">
+                            <a href="{{ URL::route('admin.advertising.index', ['id' => $item->id]) }}" title="Смотреть в админке" data-toggle="tooltip">
+                                {{ $item->title }}
+                            </a>
+                        </div>
+                    @else
+                        <div class="widget">
+                    @endif
                 @else
                     <div class="widget">
                 @endif
-            @else
-                <div class="widget">
-            @endif
-                <div class="widget-body">
-                    @include('widgets.area.controlAdvertising')
-                    @if($item->is_show_title)
-                        <h4>{{ $item->title }}</h4>
-                    @endif
-                    @if(Advertising::TYPE_ADVERTISING == $item->type)
-                        {{ $item->code }}
-                    @elseif(Advertising::TYPE_WIDGET == $item->type)
-                        <?php $sidebarWidget = app('SidebarWidget')?>
-                        {{ $sidebarWidget->show($item->code, $item->limit) }}
-                    @endif
+                    <div class="widget-body">
+                        @include('widgets.area.controlAdvertising')
+                        @if($item->is_show_title)
+                            <h4>{{ $item->title }}</h4>
+                        @endif
+                        @if(Advertising::TYPE_ADVERTISING == $item->type)
+                            {{ $item->code }}
+                        @elseif(Advertising::TYPE_WIDGET == $item->type)
+                            <?php $sidebarWidget = app('SidebarWidget')?>
+                            {{ $sidebarWidget->show($item->code, $item->limit) }}
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @else
+        @if($item->is_show_title)
+            <h4>{{ $item->title }}</h4>
+        @endif
+        {{ $item->code }}
+    @endif
 @endforeach
