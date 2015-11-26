41d674b20d5ca5d8c551fefdab25b70e5a821bf9
Annywebart <annywebart@yandex.ua>
2015-11-27 01:34:19 +0200

    Социальные кнопки: заголовок.

--- app/views/widgets/sidebar/socialButtons.blade.php
+++ app/views/widgets/sidebar/socialButtons.blade.php
@@ -1,4 +1,7 @@
 <div id="social-buttons-widget" class="sidebar-widget">
+    <span class="title-h5 margin-bottom-5 display-inline-block">
+        Понравилась статья? Поделись ею на своей странице!
+    </span>
     <script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script>
     <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,moikrug,gplus" data-yashareTheme="counter"></div>
 </div>
