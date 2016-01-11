23f7fab78b46f9633719103bd6820709072cc097
Annywebart <annywebart@yandex.ua>
2016-01-11 02:14:08 +0200

    Изменение социальных кнопок.

--- app/views/widgets/sidebar/socialButtons.blade.php
+++ app/views/widgets/sidebar/socialButtons.blade.php
@@ -4,6 +4,7 @@
             {{ $settings['socialButtonsTitle']['value'] }}
         </span>
     @endif
-    <script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script>
-    <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,moikrug,gplus" data-yashareTheme="counter"></div>
+        <script type="text/javascript" src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js" charset="utf-8"></script>
+        <script type="text/javascript" src="//yastatic.net/share2/share.js" charset="utf-8"></script>
+        <div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,gplus,twitter,lj" data-counter=""></div>
 </div>
